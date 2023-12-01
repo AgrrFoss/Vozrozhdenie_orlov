604bcd827e3de20976daf061d50459357bd11e86
Gregori Orlov <orlov.gregori@yandex.ru>
2023-12-01 23:29:05 +0300

    установить слайдеры

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,47 @@
-var sliderGallery = new Swiper('.gallery', {
+var sliderGallery = new Swiper('.photos', {
     
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  });
 
-  const sliderFeedback = new Swiper ('.swiper-feedbacks')
+    slidesPerView: 1.1,
+    spaceBetween: 20,
+    centeredSlides: true,
+    centerInsufficientSlides: true,
+    initialSlide: 1,
+    loop: true,
+    speed: 500,
+    autoplay: {
+      delay: 3000,
+    },
+    observed: true,
+    observedParents: true,
+    observedChidren: true,
+    breakpoints: {
+      900: {
+        slidesPerView: 2.5,
+        spaceBetween: 40,
+        centeredSlides: true,
+        centeredSlides: true,
+      },  
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+        centeredSlides: true,
+        centeredSlides: true,
+        
+      }
+    }
+
+    });
+
+  const sliderFeedback = new Swiper ('.feedbacks-swiper', {
+    slidesPerView: 1,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+    },
+    loop: true,
+    
+  })
